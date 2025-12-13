590059dcf220514d791b988c3b5c888e27144c80
Hugo Conceição <154693289+hugoConceicaoPT@users.noreply.github.com>
2025-12-13 17:40:20 +0000

    Pipelines fix

---
+++ backend/eslint.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint-define-config";
+
+export default defineConfig({
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "no-console": "off", // útil para backend
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+});
